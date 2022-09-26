dac34045748c217d8a3d12eb3314bb469f5dd5bc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-09-26 10:45:28 -0700

    Update lime_zinc_kitten.ex, mix.exs, and 2 more files...

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # elixir-lime-zinc-kitten
 
+## Getting started on Windows 11
+
+```
+cmd
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+mix deps.get
+scoop install cmake
+set LIBTORCH_TARGET=cu111
+mix compile
+iex.bat --werl -S mix
+```
+
+## Design
+
 * https://www.codenamegenerator.com/ with 
 * Prefix: Colors
 * Dictionary: Metal
--- lib/lime_zinc_kitten.ex
+++ lib/lime_zinc_kitten.ex
@@ -4,15 +4,42 @@ defmodule LimeZincKitten do
   """
 
   @doc """
-  Hello world.
+  Benchmark nx.
+
+  See https://github.com/elixir-nx/nx/blob/main/exla/bench/softmax.exs.
 
   ## Examples
 
-      iex> LimeZincKitten.hello()
+      iex> LimeZincKitten.bench()
       :world
 
   """
-  def hello do
-    :world
+  def bench do
+    size = 1_000_000
+    rand = for(_ <- 1..size, do: :rand.uniform())
+    t64 = Nx.tensor(rand, type: {:f, 64})
+    t32 = Nx.tensor(rand, type: {:f, 32})
+    t64_torchx = Nx.tensor(rand, type: {:f, 64}, backend: Torchx.Backend)
+    t32_torchx = Nx.tensor(rand, type: {:f, 32}, backend: Torchx.Backend)
+
+
+    # We call |> Nx.add(1) to force the computation results to be loaded
+    benches = %{
+      "elixir f32" => fn -> Softmax.softmax(t32) |> Nx.add(1) end,
+      "elixir f64" => fn -> Softmax.softmax(t64) |> Nx.add(1) end,
+      "elixir f32 torchx" => fn -> Softmax.softmax(t32_torchx) |> Nx.add(1) end,
+      "elixir f64 torchx" => fn -> Softmax.softmax(t64_torchx) |> Nx.add(1) end,
+    }
+
+    Benchee.run(
+      benches,
+      time: 10,
+      memory_time: 2
+    )
   end
+end
+
+defmodule Softmax do
+  import Nx.Defn
+  defn softmax(n), do: Nx.exp(n) / Nx.sum(Nx.exp(n))
 end
